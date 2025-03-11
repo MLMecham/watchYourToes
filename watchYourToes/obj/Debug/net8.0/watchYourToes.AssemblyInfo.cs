104608adbc3a756aa5282dc7730f620ffaed4afd
MLMecham <mec21017@byui.edu>
2025-03-11 12:10:02 -0600

    broke login

--- watchYourToes/obj/Debug/net8.0/watchYourToes.AssemblyInfo.cs
+++ watchYourToes/obj/Debug/net8.0/watchYourToes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("watchYourToes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35eae21084b2f838cdbd9c9667f96bcae36d43d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c582b4ca89d83824344c44f5ced5fc8871d305c")]
 [assembly: System.Reflection.AssemblyProductAttribute("watchYourToes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("watchYourToes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

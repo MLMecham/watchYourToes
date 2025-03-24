3e93962872c71884c60e11c35ee34578e046ad87
vbisso <valeriabisso19@gmail.com>
2025-03-24 11:23:12 -0600

    to main

--- watchYourToes/obj/Debug/net8.0/watchYourToes.AssemblyInfo.cs
+++ watchYourToes/obj/Debug/net8.0/watchYourToes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("watchYourToes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5499c1924252396d3e28b8e88c83865931cedf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f14413f37df1bf0061de005897bb926e5550ec1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("watchYourToes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("watchYourToes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

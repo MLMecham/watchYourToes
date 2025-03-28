19e4592d6979a025458d3b6bd1299847d63c1154
vbisso <valeriabisso19@gmail.com>
2025-03-28 11:46:05 -0600

    updates

--- watchYourToes/Program.cs
+++ watchYourToes/Program.cs
@@ -338,19 +338,19 @@ class Program
         
  
         // //BATTLE TEST!!!!!
-        //double check logic bc enemy still appears uu
+        // double check logic bc enemy still appears 
        
-        // Console.WriteLine("\n-- Battle Test... --\n");
-        // List<Enemy> enemies = new List<Enemy>
-        // {
-        //     new Enemy("Goblin", 10, 5, 2, 0, 1, 3, 10, new List<Gear>(), 0.5f, 1),
-        //     new Enemy("Orc", 20, 8, 5, 0, 2, 100, 20, new List<Gear>(), 0.5f, 1),
-        //     new Enemy("Skeleton", 15, 6, 3, 0, 1, 4, 15, new List<Gear>(), 0.5f, 1)
-        // };
+        Console.WriteLine("\n-- Battle Test... --\n");
+        List<Enemy> enemies = new List<Enemy>
+        {
+            new Enemy("Goblin", 10, 5, 2, 0, 1, 3, 10, new List<Gear>(), 0.5f, 1),
+            new Enemy("Orc", 20, 8, 5, 0, 2, 100, 20, new List<Gear>(), 0.5f, 1),
+            new Enemy("Skeleton", 15, 6, 3, 0, 1, 4, 15, new List<Gear>(), 0.5f, 1)
+        };
 
-        // // // // Start the battle
-        // Battle battle = new Battle(myCharacter, enemies);
-        // await battle.StartBattle();
+        // // // Start the battle
+        Battle battle = new Battle(myCharacter, enemies);
+        await battle.StartBattle();
 
 
         // //VILLAGER CHATBOT TEST!!
--- watchYourToes/obj/Debug/net8.0/watchYourToes.AssemblyInfo.cs
+++ watchYourToes/obj/Debug/net8.0/watchYourToes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("watchYourToes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e59f216bc56e4880a0c830a85426acf4c0e3f9a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+628980e9256fe7c6ff832dab091200423ad62443")]
 [assembly: System.Reflection.AssemblyProductAttribute("watchYourToes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("watchYourToes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

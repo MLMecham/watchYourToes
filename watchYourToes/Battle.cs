0c6b84d3aee37f6d096f98d1c59ab5e0223e9c33
callalilyleaf <che23035@byui.edu>
2025-03-28 19:59:11 -0600

    updated battle class to sync

--- watchYourToes/Battle.cs
+++ watchYourToes/Battle.cs
@@ -40,7 +40,7 @@ public class Battle
         // Display the combatants turn order
         
     }
-    public async Task StartBattle()
+    public void StartBattle()
     {
         Console.WriteLine($"Battle starts! {character.Name} vs {enemies.Count} enemie(s)!");
      
@@ -74,11 +74,11 @@ public class Battle
 
             if (combatant is Character charCombatant)
                 {
-                    await PlayerTurn(charCombatant);
+                    PlayerTurn(charCombatant);
                 }
             else if (combatant is Enemy enemy)
                 {
-                    await EnemyTurn(enemy);
+                    EnemyTurn(enemy);
                 }
 
             //removes dead enemies from combatants & enemies list
@@ -105,7 +105,7 @@ public class Battle
     }
 
     //player's turn - they choose who to attack
-    public async Task PlayerTurn(Character charCombatant)
+    public void PlayerTurn(Character charCombatant)
     {
         if (enemies.Count == 0) return;  //if no enemies left
 
@@ -156,11 +156,11 @@ public class Battle
             Console.WriteLine($"Remaining enemies: {enemies.Count}");
         }
 
-        await Task.Delay(1000);
+        Task.Delay(1000);
     }
 
     //enemy just attacks character, no choice
-    public async Task EnemyTurn(Enemy enemy)
+    public void EnemyTurn(Enemy enemy)
     {
         if (character.Stats.BaseStats.Health <= 0) {
             // the dead emeny already gets removed in the Battle loop
@@ -172,7 +172,7 @@ public class Battle
         Console.WriteLine($"{enemy.Name} uses its {enemy.PrintGears()} and deals {damage} damages to {character.Name}!");
         character.TakeDamage(damage);
 
-        await Task.Delay(1000);
+        Task.Delay(1000);
     }
         
 
